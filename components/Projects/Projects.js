0528011e872ead74189410ec1ec5ce4c6348019c
Angel Vazquez <angel@holofy.io>
2021-03-11 21:09:05 -0500

    dl

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -20,62 +20,111 @@ const Column = styled("div", {
   justifyContent: "flex-start",
 });
 
-const Grid = styled("div", {
-  display: "grid",
-  gridTemplateColumns: "1fr 1fr",
-  gridGap: 40,
-  alignItems: "center",
-  justifyContent: "center",
-
-  bp1: {
-    gridTemplateColumns: "1fr",
-  },
-});
-
 const Image = styled("img", {
   height: 420,
   width: "100%",
   objectfit: "cover",
-
-  ":nth-child(2)": {
-    marginTop: 160,
-
-    bp1: {
-      marginTop: 0,
-    },
-  },
-
-  ":nth-child(4)": {
-    marginTop: 160,
-
-    bp1: {
-      marginTop: 0,
-    },
-  },
 
   bp1: {
     height: 280,
   },
 });
 
-const Name = styled("p", {
-  fontSize: 44,
+const Title = styled("p", {
+  margin: 0,
+  fontSize: 30,
   fontWeight: 600,
   color: "#2b374c",
+});
+
+const Name = styled("p", {
+  margin: 0,
+  fontSize: 26,
+  fontWeight: 600,
+  color: "#2b374c",
+});
+
+const Description = styled("p", {
+  margin: 0,
+  marginTop: 18,
+  marginBottom: 40,
+  fontSize: 18,
+  fontWeight: 300,
+  color: "#989aa3",
+
+  bp1: {
+    fontSize: 18,
+    marginBottom: 40,
+  },
+});
+
+const Button = styled("button", {
+  fontSize: 16,
+  fontWeight: 600,
+  letterSpacing: 1,
+  padding: "15px 30px",
+  background: "#191d20",
+  borderRadius: 8,
+  border: "none",
+  color: "white",
+  cursor: "pointer",
+  transition: "all 0.5s linear",
+
+  ":hover": {
+    background: "#fafafa",
+    color: "#191d20",
+  },
+});
+
+const Example = styled("div", {
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr",
+  gridGap: 40,
+  marginTop: 60,
+  background: "#d1e0e6",
+
+  bp1: {
+    gridTemplateColumns: "1fr",
+  },
+});
+
+const Container = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  justifyContent: "center",
 });
 
 export default function Projects() {
   return (
     <Frame>
       <Column>
-        <Name>Last Projects</Name>
+        <Title>Last Projects</Title>
         <Fade up>
-          <Grid>
+          <Example>
             <Image src="/project03.webp" />
-            <Image src="/project04.webp" />
+            <Container>
+              <Name>Gymondo</Name>
+              <Description>
+                A pseudo-Latin text used in web design, layout, and printing in
+                place of things to emphasise layouts for previewing layouts and
+                visual mockups.{" "}
+              </Description>
+              <Button>See More &rarr;</Button>
+            </Container>
+          </Example>
+          <Example>
             <Image src="/project03.webp" />
-            <Image src="/project04.webp" />
-          </Grid>
+            <Container>
+              <Name>Gymondo</Name>
+              <Description>
+                A pseudo-Latin text used in web design, layout, and printing in
+                place of things to emphasise layouts for previewing layouts and
+                visual mockups.{" "}
+              </Description>
+              <Button>See More &rarr;</Button>
+            </Container>
+          </Example>
         </Fade>
       </Column>
     </Frame>
