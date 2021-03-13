ce8461e8a788d40a26967e862928f634d913cfe2
Angel Vazquez <angel@holofy.io>
2021-03-13 08:16:25 -0500

    sl

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -37,7 +37,7 @@ export default function Navigation() {
       <Fade>
         <Avatar>
           <Image src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80" />
-          <Name>Ángel Vázquez</Name>
+          <Name>Your Name</Name>
         </Avatar>
       </Fade>
     </Frame>
